1d4c9905f1ca0cfadd52e5bb447b9ffc12b4ef4a
varitant <varitanta@gmail.com>
2023-12-03 16:16:08 +0530

    design added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import UserList from "./components/UserList";
 
 function App() {
   return (
-    <>
+    <div className="bg-slate-50 min-h-screen p-20 pt-10">
+      <h1 className="text-gray-600 text-4xl font-bold text-center m-4">ADMIN DASHBOARD</h1>
       <UserList/>
-    </>
+    </div>
   );
 }
 
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { FaSearch, FaEdit, FaTrash, FaTimes, FaCheck } from "react-icons/fa";
+import {
+  FaSearch,
+  FaEdit,
+  FaTrash,
+  FaTimes,
+  FaCheck,
+  FaFastBackward,
+  FaBackward,
+  FaForward,
+  FaFastForward,
+  FaRegCheckSquare,
+} from "react-icons/fa";
+import { LuDivideCircle, LuTrash2 } from "react-icons/lu";
+import { MdCancelPresentation } from "react-icons/md";
 
 const UserList = () => {
-  const usersPerPage = 10;
   const [users, setUsers] = useState([]);
   const [filteredUsers, setFilteredUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedRows, setSelectedRows] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [editableUser, setEditableUser] = useState(null);
+  const [usersPerPage, setUsersPerPage] = useState(10);
   const [editedUserData, setEditedUserData] = useState({
     name: "",
     email: "",
@@ -41,6 +54,7 @@ const UserList = () => {
       )
     );
     setFilteredUsers(filtered);
+    setSelectedRows([]);
     setCurrentPage(1);
   };
 
@@ -105,82 +119,108 @@ const UserList = () => {
     const pagesToShow = 1;
     const pageNumbers = [];
     for (let i = 1; i <= totalPages; i++) {
-      if (
-        i === 1 ||
-        i === currentPage ||
-        (i >= currentPage - pagesToShow && i <= currentPage + pagesToShow) ||
-        i === totalPages
-      ) {
-        pageNumbers.push(
-          <button
-            key={i}
-            onClick={() => setCurrentPage(i)}
-            className={`page-number mx-1 px-3 py-1 border ${
-              i === currentPage ? "bg-blue-500 text-white" : ""
-            }`}
-          >
-            {i}
-          </button>
-        );
-      }
+      pageNumbers.push(
+        <button
+          key={i}
+          onClick={() => setCurrentPage(i)}
+          className={`page-number px-3 py-1 border border-gray-300 ${
+            i === currentPage ? "bg-blue-500 text-white" : ""
+          }`}
+        >
+          {i}
+        </button>
+      );
     }
     return pageNumbers;
   };
   return (
-    <div className="container mx-auto p-4">
-      <div className="mb-4">
-        <form onSubmit={handleSearch}>
+    <div className="container mx-auto p-8 bg-white shadow-[0_15px_20px_0px_rgba(0,0,0,0.1)] font-arial text-slate-600">
+      <div className="mb-4 flex justify-between">
+        <div className="flex items-center">
+          <label className="mx-2">Show</label>
+          <select
+            value={usersPerPage}
+            onChange={(e) => setUsersPerPage(e.target.value)}
+            className="border py-1 px-4 bg-slate-50 focus:outline-none focus:ring focus:border-blue-100"
+          >
+            <option value={10}>10</option>
+            <option value={15}>15</option>
+            <option value={20}>20</option>
+          </select>
+          <label className="m-2">entries</label>
+        </div>
+        <form className="m-1" onSubmit={handleSearch}>
           <input
             type="text"
             placeholder="Search"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="border p-2 mr-2"
+            className="h-full border py-1 px-4 bg-slate-50 focus:outline-none focus:ring focus:border-blue-100"
           />
-          <button type="submit" className="search-icon bg-blue-500 text-white p-2">
+          <button
+            type="submit"
+            className="search-icon text-xl px-4"
+          >
             <FaSearch />
           </button>
         </form>
+        <div className="delete-selected">
+          <button
+            onClick={handleDeleteSelected}
+            disabled={selectedRows.length===0}
+            className={`text-red-500 border border-red-500 ${selectedRows.length>0 && "hover:text-white hover:bg-red-500 "} rounded p-2`}
+          >
+            Delete Selected
+          </button>
+        </div>
       </div>
-      <table className="w-full border">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="p-2">
+
+      <table className="w-full">
+        <thead className="w-full">
+          <tr className="bg-gray-200 text-left border-y grid grid-cols-12">
+            <th className="p-3 pt-3.5 pb-2.5 ">
               <input
                 type="checkbox"
-                checked={selectedRows.length === currentUsers.length}
+                checked={
+                  selectedRows.length === currentUsers.length &&
+                  selectedRows.length > 0
+                }
                 onChange={handleToggleSelectAll}
-                className="form-checkbox h-5 w-5 text-blue-500"
+                className="form-checkbox h-4 w-4 text-blue-500"
               />
             </th>
-            <th className="p-2">ID</th>
-            <th className="p-2">Name</th>
-            <th className="p-2">Email</th>
-            <th className="p-2">Role</th>
-            <th className="p-2">Actions</th>
+            <th className="p-3 col-span-3">Name</th>
+            <th className="p-3 col-span-4">Email</th>
+            <th className="p-3 col-span-2">Role</th>
+            <th className="p-3 col-span-2">Actions</th>
           </tr>
         </thead>
         <tbody>
           {currentUsers.map((user) => (
             <tr
               key={user.id}
-              className={selectedRows.includes(user.id) ? "bg-gray-100" : ""}
+              className={`text-left border-b grid grid-cols-12 hover:bg-gray-100 ${
+                selectedRows.includes(user.id) ? "bg-gray-100" : ""
+              }`}
             >
-              <td className="p-2">
+              <td className="p-3 pt-3.5 pb-2.5">
                 <input
                   type="checkbox"
                   checked={selectedRows.includes(user.id)}
                   onChange={() => handleToggleSelect(user.id)}
-                  className="form-checkbox h-5 w-5 text-blue-500"
+                  className="form-checkbox h-4 w-4 text-blue-500"
                 />
               </td>{" "}
-              <td className="p-2">{user.id}</td>
-              <td className="p-2 ">
+              <td
+                className={`p-3 col-span-3 ${
+                  editableUser === user.id && "p-0"
+                }`}
+              >
                 {editableUser === user.id ? (
                   <input
                     type="text"
                     value={editedUserData.name}
-                    className="w-32"
+                    className="w-full h-full p-3 border bg-slate-50 focus:outline-none focus:ring ring-inset focus:border-blue-100"
                     onChange={(e) =>
                       setEditedUserData({
                         ...editedUserData,
@@ -192,12 +232,16 @@ const UserList = () => {
                   user.name
                 )}
               </td>
-              <td className="p-2">
+              <td
+                className={`p-3 col-span-4 ${
+                  editableUser === user.id && "p-0"
+                }`}
+              >
                 {editableUser === user.id ? (
                   <input
                     type="text"
                     value={editedUserData.email}
-                    className="w-32"
+                    className="w-full h-full p-3 border bg-slate-50 focus:outline-none focus:ring ring-inset focus:border-blue-100"
                     onChange={(e) =>
                       setEditedUserData({
                         ...editedUserData,
@@ -209,49 +253,59 @@ const UserList = () => {
                   user.email
                 )}
               </td>
-              <td className="p-2">
+              <td
+                className={`p-3 col-span-2 capitalize ${
+                  editableUser === user.id && "p-0"
+                }`}
+              >
                 {editableUser === user.id ? (
-                  <input
-                    type="text"
+                  <select
+                    type="select"
                     value={editedUserData.role}
-                    className="w-32"
+                    className="w-full h-full p-3 border bg-slate-50 focus:outline-none focus:ring ring-inset focus:border-blue-100"
                     onChange={(e) =>
                       setEditedUserData({
                         ...editedUserData,
                         role: e.target.value,
                       })
                     }
-                  />
+                  >
+                    <option value="member">Member</option>
+                    <option value="admin">Admin</option>
+                  </select>
                 ) : (
                   user.role
                 )}
               </td>
-              <td className="p-2">
+              <td className="p-3 col-span-2">
                 {editableUser === user.id ? (
                   <>
                     <button
-                      className="save mr-2"
+                      className="save mr-2 text-green-500 text-xl"
                       onClick={() => handleSave(user.id, editedUserData)}
                     >
-                      <FaCheck />
+                      <FaRegCheckSquare />
                     </button>
-                    <button className="cancel mr-2" onClick={handleCancel}>
-                      <FaTimes />
+                    <button
+                      className="cancel mx-2 text-red-500 text-xl"
+                      onClick={handleCancel}
+                    >
+                      <MdCancelPresentation />
                     </button>
                   </>
                 ) : (
                   <>
                     <button
                       onClick={() => handleEdit(user.id)}
-                      className="edit text-blue-500 mr-2"
+                      className="edit mr-2"
                     >
                       <FaEdit />
                     </button>
                     <button
                       onClick={() => handleDelete(user.id)}
-                      className="delete text-red-500"
+                      className="delete m-2 text-red-500"
                     >
-                      <FaTrash />
+                      <LuTrash2 />
                     </button>
                   </>
                 )}
@@ -260,48 +314,55 @@ const UserList = () => {
           ))}
         </tbody>
       </table>
-      <div className="mt-4 flex justify-between items-center">
-        <div>
+      <div className="mt-2 flex justify-between items-center">
+        {currentUsers.length > 0 ? (
+          <div className="m-2">
+            Showing {indexOfFirstUser + 1} to{" "}
+            {currentUsers.length + indexOfFirstUser} of {filteredUsers.length}{" "}
+            entries
+          </div>
+        ) : (
+          <div className="m-2">No Users</div>
+        )}
+        <div className="m-2 text-gray-400">
           <button
             onClick={() => setCurrentPage(1)}
             disabled={currentPage === 1}
-            className="first-page text-blue-500"
+            className={`first-page px-3 py-1 border border-gray-300  ${
+              currentPage === 1 && "bg-gray-200"
+            }`}
           >
-            First Page
+            First
           </button>
           <button
             onClick={() => setCurrentPage(currentPage - 1)}
             disabled={currentPage === 1}
-            className="previous-page text-blue-500 mx-2"
+            className={`previous-page px-3 py-1 border border-gray-300 ${
+              currentPage === 1 && "bg-gray-200"
+            }`}
           >
-            Previous Page
+            Previous
           </button>
-        </div>
-        <div>{renderPageNumbers()}</div>
-        <div>
+          {renderPageNumbers()}
           <button
             onClick={() => setCurrentPage(currentPage + 1)}
             disabled={currentPage === totalPages}
-            className="next-page text-blue-500 mx-2"
+            className={`next-page px-3 py-1 border border-gray-300 ${
+              currentPage === totalPages && "bg-gray-200"
+            }`}
           >
-            Next Page
+            Next
           </button>
           <button
             onClick={() => setCurrentPage(totalPages)}
             disabled={currentPage === totalPages}
-            className="last-page text-blue-500"
+            className={`last-page px-3 py-1 border border-gray-300 ${
+              currentPage === totalPages && "bg-gray-200"
+            }`}
           >
-            Last Page
+            Last
           </button>
         </div>
-      </div>
-      <div className="mt-4 delete-selected">
-        <button
-          onClick={handleDeleteSelected}
-          className="bg-red-500 text-white p-2"
-        >
-          Delete Selected
-        </button>
       </div>
     </div>
   );
